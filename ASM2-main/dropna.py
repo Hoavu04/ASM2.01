bfd2b030dd429f198ea3b140edcb29f1f99af609
Hoavu04 <166799471+Hoavu04@users.noreply.github.com>
2024-08-14 18:27:55 +0700

    Delete ASM2-main directory

--- ASM2-main/dropna.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df = pd.read_excel("Sales_Data.xlsx", sheet_name="Sheet1")
-pd.set_option('display.max_rows', 120)
-df= df.dropna()
-print(df)
--- ASM2-main/filterdata.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df = pd.read_excel("Sales_Data.xlsx", sheet_name="Sheet2")
-pd.set_option('display.max_rows', 120)
-filltered_df = df[df['Order Quantity'] >11]
-print(filltered_df)
